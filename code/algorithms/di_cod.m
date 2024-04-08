3f5baa93d4ea1261672643290f71b36e1c1f2e1a
monstervoice <57940138+monstervoice@users.noreply.github.com>
2024-04-08 11:26:50 +0800

    Update di_cod.m

--- code/algorithms/di_cod.m
+++ code/algorithms/di_cod.m
@@ -1,9 +1,7 @@
-function [l_avg, l_ans, err_max, cost] = di_cod(X, Y, L_number, N, gap, flag_time)
+function [l_avg, l_ans, err_max, cost] = di_cod(X, Y, L_number, N, gap, flag_time, RX, RY)
     tic;
     [mx, n] = size(X);
     my = size(Y, 1);
-    RX = 773;
-    RY = 618;
     l_avg = 0;
     err_avg = 0;
     err_max = 0;
